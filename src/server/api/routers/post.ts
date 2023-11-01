e6740a6cd603d7899be3c393a5f9ba23a54f8ad7
som3aware <ahmed.ismail@nodogoro.com>
2023-11-01 18:20:27 +0200

    fix: remove async from generateAdVariant

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -8,28 +8,6 @@ let post = {
 };
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      post = { id: post.id + 1, name: input.name };
-      return post;
-    }),
-
-  getLatest: publicProcedure.query(() => {
-    return post;
-  }),
-
   generateAdVariant: publicProcedure
     .input(
       z.object({
@@ -38,7 +16,7 @@ export const postRouter = createTRPCRouter({
         image: z.string(),
       }),
     )
-    .mutation(async ({ input }) => {
+    .mutation(({ input }) => {
       return { img1x1: input.image, img9x16: input.image, variations: `${input.brief}\n${input.platforms}` };
     }),
 });
